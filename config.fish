2c0306530e51e46e6600c02e4bd189b0b7977ebd
David Tsai <david@whataniceguy.com>
2018-03-06 09:55:40 -0800

    Adding support for rabbitMQ scripts

--- config.fish
+++ config.fish
@@ -1,10 +1,17 @@
 source ~/.config/fish/aliases.fish
 
+# https://syapse.atlassian.net/wiki/spaces/SE/pages/10453004/How+to+install+RabbitMQ+On+Mac
+# RabbitMQ server scripts are installed into /usr/local/sbin
+# Here we modify the $PATH using the `fish` universal variables
+set -U fish_user_paths /usr/local/sbin $PATH
+
+# To enable our vi-like keybindings, :remap <ESC> 'kj'
 set fish_key_bindings fish_user_key_bindings
 
 # If we make modifications to path, load `virtualfish` after
 eval (python -m virtualfish projects auto_activation compat_aliases)
 
+# Custom prompt to display current virtualenv
 if set -q VIRTUAL_ENV
   echo -n -s (set_color -b blue white) "(" (basename "$VIRTUAL_ENV") ")" (set_color normal) " "
 end
@@ -18,5 +25,6 @@ function postactivate --on-event virtualenv_did_activate
   test -e $postactivate; and source $postactivate
 end
 
+# Do I still need this?
 set PROJECT_HOME $HOME/work
 set WORKON_HOME $HOME/work
